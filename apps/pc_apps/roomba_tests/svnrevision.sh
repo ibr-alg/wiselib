7521fc85b3934b5cc40f22ed6090ba2c9029780b
Roland Hieber <rohieb@rohieb.name>
2011-07-30 03:31:12 +0200

    also look for plain git revision

--- apps/pc_apps/roomba_tests/svnrevision.sh
+++ apps/pc_apps/roomba_tests/svnrevision.sh
@@ -6,11 +6,20 @@ stat=$(svn status 2>&1|grep '^M')
 
 # if that fails, try git-svn
 if [ -z "$revision" ]; then
-  revision=$(git svn info 2>&1|grep Revision|sed 's/Revision: //')
-  stat=$(git status 2>&1|grep 'modified: ')
-  # ...and also check if there are un-dcommited changesets
-  stat=$stat$(git diff --stat HEAD git-svn); 
+  if [ -n "`git svn info 2>/dev/null`" ]; then
+    revision=$(git svn info 2>&1|grep Revision|sed 's/Revision: //')
+    stat=$(git status 2>&1|grep 'modified: ')
+    # ...and also check if there are un-dcommited changesets
+    stat=$stat$(git diff --stat HEAD git-svn); 
+  fi;
 fi;
+
+# if that fails, try git
+if [ -z "$revision" ]; then
+  revision=$(git rev-parse HEAD)
+  stat=$(git status 2>&1|grep 'modified: ')
+fi;
+
 
 # if that fails, give up
 if [ -z "$revision" ]; then
@@ -24,5 +33,5 @@ else
 fi;
 
 echo '// This file is created automatically, your changes will be lost.' > svnrevision.h
-echo '#define SVNREVISION "r'$revision$status'"'  >> svnrevision.h
+echo '#define SVNREVISION "'$revision$status'"'  >> svnrevision.h
 
