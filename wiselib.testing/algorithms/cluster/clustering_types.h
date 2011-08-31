2393473c64065434e72c8457f665dff6562f94e1
Dimitrios Amaxilatis <amaxilat@cti.gr>
2011-08-31 13:05:49 +0300

    move messages to messages folder

--- wiselib.testing/algorithms/cluster/clustering_types.h
+++ wiselib.testing/algorithms/cluster/clustering_types.h
@@ -63,21 +63,21 @@ namespace wiselib {
 #include "algorithms/cluster/semantics.h" 
 
 
-#include "algorithms/cluster/join_message.h"
+#include "algorithms/cluster/messages/join_message.h"
 typedef wiselib::JoinClusterMsg<wiselib::OSMODEL, wiselib::OSMODEL::Radio> JoinClusterMsg_t;
 //#include "algorithms/cluster/messages/head_lost.h"
 //typedef wiselib::LostClusterMsg<wiselib::OSMODEL, wiselib::OSMODEL::Radio> LostClusterMsg_t;
 #include "algorithms/cluster/messages/join_sema.h"
 typedef wiselib::JoinSemanticClusterMsg<wiselib::OSMODEL, wiselib::OSMODEL::Radio> JoinSemanticClusterMsg_t;
-#include "algorithms/cluster/join_multiple_message.h"
+#include "algorithms/cluster/messages/join_multiple_message.h"
 typedef wiselib::JoinMultipleClusterMsg<wiselib::OSMODEL, wiselib::OSMODEL::Radio> JoinMultipleClusterMsg_t;
-#include "algorithms/cluster/convergecast_message.h"
+#include "algorithms/cluster/messages/convergecast_message.h"
 typedef wiselib::ConvergecastMsg<wiselib::OSMODEL, wiselib::OSMODEL::Radio> ConvergecastMsg_t;
-#include "algorithms/cluster/join_accept_message.h"
-#include "algorithms/cluster/resume_message.h"
+#include "algorithms/cluster/messages/join_accept_message.h"
+#include "algorithms/cluster/messages/resume_message.h"
 typedef wiselib::ResumeClusterMsg<wiselib::OSMODEL, wiselib::OSMODEL::Radio> ResumeMsg_t;
-#include "algorithms/cluster/reform_message.h"
-#include "algorithms/cluster/attribute_message.h"
+#include "algorithms/cluster/messages/reform_message.h"
+#include "algorithms/cluster/messages/attribute_message.h"
 
 
 #include "algorithms/cluster/messages/sema_attr.h"
