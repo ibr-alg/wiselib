d9835b5a4c56bc666e364ef93a9f4e2405d037b2
Jan Kokemüller <jan.kokemueller@gmail.com>
2012-02-13 14:57:30 +0000

    reuse iterator tags from std namespace when there is a STL available
    Fixes compile error when using Boost concept checking.

--- wiselib.stable/util/pstl/iterator_base_types.h
+++ wiselib.stable/util/pstl/iterator_base_types.h
@@ -23,12 +23,19 @@
 
 namespace wiselib
 {
-
+#ifdef __unix__
+    typedef std::input_iterator_tag          input_iterator_tag;
+    typedef std::output_iterator_tag         output_iterator_tag;
+    typedef std::forward_iterator_tag        forward_iterator_tag;
+    typedef std::bidirectional_iterator_tag  bidirectional_iterator_tag;
+    typedef std::random_access_iterator_tag  random_access_iterator_tag;
+#else
     struct input_iterator_tag {};
     struct output_iterator_tag {};
     struct forward_iterator_tag : public input_iterator_tag {};
     struct bidirectional_iterator_tag : public forward_iterator_tag {};
     struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+#endif
     // -----------------------------------------------------------------------
     // -----------------------------------------------------------------------
     // -----------------------------------------------------------------------
