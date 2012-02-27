0a87d9610055d8967eec4cbbad27b567dde8a56a
Stephan Lohse <lohse@itm.uni-luebeck.de>
2012-02-27 16:53:15 +0100

    added PCOsModel::Os because ComISenseRadioModel is looking for it added operator()(value_t max) to PCRandModel in order to be compatible to other rand models

--- wiselib.testing/external_interface/pc/pc_rand.h
+++ wiselib.testing/external_interface/pc/pc_rand.h
@@ -30,6 +30,7 @@ namespace wiselib {
 			PCRandModel(value_t seed);
 			void srand(value_t seed);
 			value_t operator()();
+			value_t operator()(value_t max);
 			int state();
 			
 		private:
@@ -57,6 +58,12 @@ namespace wiselib {
 	operator()() {
 		return rand();
 	}
+
+	template<typename OsModel_P>
+	typename PCRandModel<OsModel_P>::value_t PCRandModel<OsModel_P>::
+	operator()( value_t max ) {
+	        return ( rand() % max );
+	}	
 	
 	template<typename OsModel_P>
 	int PCRandModel<OsModel_P>::
