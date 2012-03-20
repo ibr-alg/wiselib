ffc00fdecf97df03ceec74a50013142d10cdacec
Konstantinos Chantzis <chantzis.konstantinos@googlemail.com>
2012-03-20 15:32:40 +0100

    updated privacy module

--- wiselib.testing/algorithms/privacy/privacy_config.h
+++ wiselib.testing/algorithms/privacy/privacy_config.h
@@ -1,25 +1,8 @@
-/***************************************************************************
- ** This file is part of the generic algorithm library Wiselib.           **
- ** Copyright (C) 2008,2009 by the Wisebed (www.wisebed.eu) project.      **
- **                                                                       **
- ** The Wiselib is free software: you can redistribute it and/or modify   **
- ** it under the terms of the GNU Lesser General Public License as        **
- ** published by the Free Software Foundation, either version 3 of the    **
- ** License, or (at your option) any later version.                       **
- **                                                                       **
- ** The Wiselib is distributed in the hope that it will be useful,        **
- ** but WITHOUT ANY WARRANTY; without even the implied warranty of        **
- ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
- ** GNU Lesser General Public License for more details.                   **
- **                                                                       **
- ** You should have received a copy of the GNU Lesser General Public      **
- ** License along with the Wiselib.                                       **
- ** If not, see <http://www.gnu.org/licenses/>.                           **
- ***************************************************************************/
-
-#define ISENSE_PRIVACY_DEBUG
+#define SHAWN_PRIVACY_PC_APPLICATION
+//#define PRIVACY_DEBUG
 #define PRIVACY_CIPHER_TEXT_MAX_SIZE 80
-//#define PRIVACY_ENABLE_ENCRYPTION
-//#define PRIVACY_ENABLE_DECRYPTION
-//#define PRIVACY_ENABLE_RANDOMIZATION
+#define PRIVACY_ENABLE_ENCRYPTION
+#define PRIVACY_ENABLE_DECRYPTION
+#define PRIVACY_ENABLE_RANDOMIZATION
+#define PRIVACY_POWER_DB 0
 
