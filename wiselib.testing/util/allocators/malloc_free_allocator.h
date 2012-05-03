40d258ee509205fc28945eec8d73ff005c59bfb9
Henning Hasemann <henning@leetless.de>
2012-05-03 20:01:37 +0200

    hotfix malloc_free_allocator for isense

--- wiselib.testing/util/allocators/malloc_free_allocator.h
+++ wiselib.testing/util/allocators/malloc_free_allocator.h
@@ -27,7 +27,7 @@
 void* operator new(size_t size, pointer_t ptr) {
 	return ptr.raw();
 }*/
-void* operator new(size_t size, void* ptr) {
+void* operator new(size_t size, void* ptr, bool fu_isense) {
 	return ptr;
 }
 
@@ -120,7 +120,7 @@ class MallocFreeAllocator {
 				void *p = malloc(sizeof(T));
 			#endif
 			//new(r) T;
-			new(p) T;
+			new(p, true) T;
 			pointer_t<T> r(reinterpret_cast<T*>(p));
 			return r;
 		}
@@ -138,7 +138,7 @@ class MallocFreeAllocator {
 			array_pointer_t<T> r(reinterpret_cast<T*>(p), n);
 			for(typename OsModel::size_t i = 0; i < n; i++) {
 				//new(pointer_t<T>(&(r.raw()[i]))) T;
-				new(&(reinterpret_cast<T*>(p)[i])) T;
+				new(&(reinterpret_cast<T*>(p)[i]), true) T;
 			}
 			return r;
 		}
