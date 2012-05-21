53a848e0f48968d3ab221d0579a057365680f5a1
wiselib <daniel.gehberger@gmail.com>
2012-05-21 21:54:47 +0200

    Bitwise serialization util added, IPv6 packet added

--- wiselib.testing/algorithms/6lowpan/ipv6_address.h
+++ wiselib.testing/algorithms/6lowpan/ipv6_address.h
@@ -23,75 +23,98 @@
 
 namespace wiselib
 {
-	template<typename OsModel_P,typename Debug_P>
+	template<typename Debug_P>
 	class IPv6Address
 	{
 	public:
-		typedef OsModel_P OsModel;
-		typedef Debug_P Debug;
-		
-		IPv6Address()
+	
+	typedef Debug_P Debug;
+	
+	IPv6Address()
+	{
+	}
+	
+	void set_debug( Debug& debug )
+	{
+		debug_ = &debug;
+	}
+	
+	// --------------------------------------------------------------------
+	
+	void set_address( uint8_t* address )
+	{
+		memcpy(&(addr_[0]), address, 16);
+	}
+	
+	// --------------------------------------------------------------------
+	
+	void set_prefix( uint8_t* prefix )
+	{
+		memcpy(&(addr_[0]), prefix, 8);
+	}
+	
+	// --------------------------------------------------------------------
+	
+	void set_iid( uint8_t* iid )
+	{
+		memcpy(&(addr_[8]), iid, 8);
+	}
+	
+	// --------------------------------------------------------------------
+	
+	//MAC: 48-bit-long --> 6 bytes
+	//To construct EUI-64: 1. 2. 3. FF FE 4. 5. 6.
+	//U bit: in the first byte: _ _ _ _ _ _ 1 _
+	
+	//TODO: uint8_t doesn't seem so good for node_id_t
+	void set_iid_from_MAC( uint8_t* ll_addr )
+	{
+		memcpy(&(addr_[8]), ll_addr, 3);
+		addr_[11]=0xFF;
+		addr_[12]=0xFE;
+		memcpy(&(addr_[13]), ll_addr+3, 3);
+		addr_[8] |= 0x02;
+	}
+	
+	// --------------------------------------------------------------------
+	
+	void make_it_link_local()
+	{
+		uint8_t link_local_prefix[8];
+		link_local_prefix[0]=0xFE;
+		link_local_prefix[1]=0x80;
+		memset(&(link_local_prefix[2]),0, 6);
+		set_prefix(link_local_prefix);
+	}
+	
+	// --------------------------------------------------------------------
+	
+	void print_address()
+	{
+		debug().debug( "Address: " );
+		for(uint8_t i = 0; i < 16; i++)
 		{
+			debug().debug( "%x", addr_[i] >> 4 );
+			debug().debug( "%x", addr_[i] & 0x0F );
+			if(i%2==1 && i<15)
+				debug().debug( ":" );
 		}
-		
-		// --------------------------------------------------------------------
-		
-		void set_prefix( uint8* prefix )
-		{
-			memcpy(&(addr_[0]), prefix, 8);
-		}
-		
-		// --------------------------------------------------------------------
-		
-		void set_iid( uint8* iid )
-		{
-			memcpy(&(addr_[8]), iid, 8);
-		}
-		
-		// --------------------------------------------------------------------
-		
-		//MAC: 48-bit-long --> 6 bytes
-		//To construct EUI-64: 1. 2. 3. FF FE 4. 5. 6.
-		//U bit: in the first byte: _ _ _ _ _ _ 1 _
-		void set_iid_from_MAC( uint8* ll_addr )
-		{
-			memcpy(&(addr_[8]), ll_addr, 3);
-			addr_[11]=0xFF;
-			addr_[12]=0xFE;
-			memcpy(&(addr_[13], ll_addr+3, 3);
-			addr_[8] |= 0x02;
-		}
-		
-		// --------------------------------------------------------------------
-		
-		void make_it_link_local()
-		{
-			uint8 link_local_prefix[8];
-			link_local_prefix[0]=0xFE;
-			link_local_prefix[1]=0x80;
-			memset(&(link_local_prefix[2]),0, 6);
-			set_prefix(link_local_prefix);
-		}
-		
-		// --------------------------------------------------------------------
-		
-		void print_address()
-		{
-			debug().debug( "Address: " );
-			for(int i = 0; i < 16; i++)
-			{
-				//TODO: a clean method to print in HEX
-				debug().debug( addr_[i] >> 4 );
-				debug().debug( addr_[i] &= 0x0F );
-				if(i%2==0 && i>0)
-					debug().debug( ":" );
-			}
-			debug().debug( "\n" );
-		}
+		debug().debug( "\n" );
+	}
+	
+	uint8_t* get_address()
+	{
+		return addr_;
+	}
 	
 	  
 	private:
-		uint8 addr_[16];
-   }
+	
+	Debug& debug()
+	{ return *debug_; }
+	
+	uint8_t addr_[16];
+	typename Debug::self_pointer_t debug_;
+   };
 }
 #endif
---
+++ wiselib.testing/algorithms/6lowpan/ipv6_packet.h
@@ -0,0 +1,239 @@
+/***************************************************************************
+ ** This file is part of the generic algorithm library Wiselib.           **
+ ** Copyright (C) 2008,2009 by the Wisebed (www.wisebed.eu) project.      **
+ **                                                                       **
+ ** The Wiselib is free software: you can redistribute it and/or modify   **
+ ** it under the terms of the GNU Lesser General Public License as        **
+ ** published by the Free Software Foundation, either version 3 of the    **
+ ** License, or (at your option) any later version.                       **
+ **                                                                       **
+ ** The Wiselib is distributed in the hope that it will be useful,        **
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+ ** GNU Lesser General Public License for more details.                   **
+ **                                                                       **
+ ** You should have received a copy of the GNU Lesser General Public      **
+ ** License along with the Wiselib.                                       **
+ ** If not, see <http://www.gnu.org/licenses/>.                           **
+ ***************************************************************************/
+#ifndef __ALGORITHMS_6LOWPAN_IPV6_PACKET_H__
+#define __ALGORITHMS_6LOWPAN_IPV6_PACKET_H__
+
+#include "algorithms/6lowpan/ipv6_address.h"
+#include "util/serialization/bitwise_serialization.h"
+
+/*
+  IPv6 Header
+    1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8 1 2 3 4 5 6 7 8
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |Ver(4) | Tr Class (8)  |           Flow Label(20)              |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |       Payload Length(16)      | Next Header(8)|  Hop Limit(8) |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                                                               |
+   +                                                               +
+   |                                                               |
+   +                         Source Address                        +
+   |                               (128)                           |
+   +                                                               +
+   |                                                               |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+   |                                                               |
+   +                                                               +
+   |                                                               |
+   +                      Destination Address                      +
+   |                               (128)                           |
+   +                                                               +
+   |                                                               |
+   +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  
+  Default values
+    * Version = 6 (FIX)
+    * Traffic Class = 0
+    * Flow Label = 0
+
+*/
+namespace wiselib
+{
+	
+	
+	template<typename OsModel_P,
+		typename Radio_P,
+		typename Debug_P>
+	class IPv6Packet
+	{
+	public:
+	typedef Debug_P Debug;
+	typedef OsModel_P OsModel;
+	//typedef Radio_P Radio;
+	//TODO: from the radio?
+	typedef uint8_t block_data_t;
+	
+	
+	typedef IPv6Address<Debug> IPv6Address_t;
+	
+	IPv6Packet()
+	{
+	}
+	
+	void set_debug( Debug& debug )
+	{
+		debug_ = &debug;
+	}
+	
+	
+	void init( uint8_t next_header, uint8_t hop_limit, uint16_t length, uint8_t* payload, IPv6Address_t* source, IPv6Address_t* destination, uint8_t traffic_class=0, uint32_t flow_label=0 )
+	{
+		memset(header_, 0, 40);
+		
+		//Version
+		//header_[0] |= 6 << 4;
+		uint8_t version = 6;
+		bitwise_write<OsModel, block_data_t, uint8_t>( header_ + VERSION_BYTE, version, VERSION_BIT, VERSION_LEN );
+		
+		//Traffic Class
+		//header_[0] |= traffic_class >> 4;
+		//header_[1] |= traffic_class << 4;
+		bitwise_write<OsModel, block_data_t, uint8_t>( header_ + TRAFFIC_CLASS_BYTE, traffic_class, TRAFFIC_CLASS_BIT, TRAFFIC_CLASS_LEN );
+		
+		//Flow Label
+		//header_[1] |= flow_label >> 16;
+		//header_[2] |= flow_label >> 8;
+		//header_[3] |= flow_label;
+		bitwise_write<OsModel, block_data_t, uint32_t>( header_ + FLOW_LABEL_BYTE, flow_label, FLOW_LABEL_BIT, FLOW_LABEL_LEN );
+		
+		//Length
+		//header_[4] |= length >> 8;
+		//header_[5] |= length;
+		bitwise_write<OsModel, block_data_t, uint16_t>( header_ + LENGTH_BYTE, length, LENGTH_BIT, LENGTH_LEN );
+		
+		//Next Header
+		//header_[6] |= next_header;
+		bitwise_write<OsModel, block_data_t, uint8_t>( header_ + NEXT_HEADER_BYTE, next_header, NEXT_HEADER_BIT, NEXT_HEADER_LEN );
+		
+		//Hop limit
+		//header_[7] |= hop_limit;
+		bitwise_write<OsModel, block_data_t, uint8_t>( header_ + HOP_LIMIT_BYTE, hop_limit, HOP_LIMIT_BIT, HOP_LIMIT_LEN );
+		
+		//Source address
+		uint8_t* addr_pointer;
+		addr_pointer=source->get_address();
+		memcpy((header_ + SOURCE_ADDRESS_BYTE), addr_pointer, 16);
+		
+		//Destination address
+		addr_pointer=destination->get_address();
+		memcpy((header_ + DESTINATION_ADDRESS_BYTE), addr_pointer, 16);
+		
+		//Payload
+		payload_=payload;
+	}
+	
+	inline uint8_t version()
+	{
+		return bitwise_read<OsModel, block_data_t, uint8_t>( header_ + VERSION_BYTE, VERSION_BIT, VERSION_LEN );
+	}
+	
+	inline uint8_t traffic_class()
+	{
+		return bitwise_read<OsModel, block_data_t, uint8_t>( header_ + TRAFFIC_CLASS_BYTE, TRAFFIC_CLASS_BIT, TRAFFIC_CLASS_LEN );
+	}
+	
+	inline uint32_t flow_label()
+	{
+		return bitwise_read<OsModel, block_data_t, uint32_t>( header_ + FLOW_LABEL_BYTE, FLOW_LABEL_BIT, FLOW_LABEL_LEN );
+	}
+	
+	inline uint16_t length()
+	{
+		return bitwise_read<OsModel, block_data_t, uint16_t>( header_ + LENGTH_BYTE, LENGTH_BIT, LENGTH_LEN );
+	}
+	
+	inline uint8_t next_header()
+	{
+		return bitwise_read<OsModel, block_data_t, uint8_t>( header_ + NEXT_HEADER_BYTE, NEXT_HEADER_BIT, NEXT_HEADER_LEN );
+	}
+	
+	inline uint8_t hop_limit()
+	{
+		return bitwise_read<OsModel, block_data_t, uint8_t>( header_ + HOP_LIMIT_BYTE, HOP_LIMIT_BIT, HOP_LIMIT_LEN );
+	}
+	
+	inline void source_address(IPv6Address_t& address)
+	{
+		uint8_t tmp_address[16];
+		memcpy(tmp_address, (header_ + SOURCE_ADDRESS_BYTE) ,16);
+		address.set_address(tmp_address);
+	}
+	
+	inline void destination_address(IPv6Address_t& address)
+	{
+		uint8_t tmp_address[16];
+		memcpy(tmp_address, (header_ + DESTINATION_ADDRESS_BYTE) ,16);
+		address.set_address(tmp_address);
+	}
+	
+	void print_header()
+	{
+		debug().debug( "Version: %d \n", version());
+		debug().debug( "Traffic Class: %d \n", traffic_class());
+		debug().debug( "Flow Label: %d \n", flow_label());
+		debug().debug( "Length: %d \n", length());
+		debug().debug( "Next Header: %d \n", next_header());
+		debug().debug( "Hop Limit: %d \n", hop_limit());
+		
+		IPv6Address_t addr;
+		source_address( addr );
+		debug().debug( "Source Address: ");
+		addr.print_address();
+		
+		destination_address( addr );
+		debug().debug( "Destination Address: ");
+		addr.print_address();
+	}
+	
+	enum position_starts_byte
+	{
+	  VERSION_BYTE = 0,
+	  TRAFFIC_CLASS_BYTE = 0,
+	  FLOW_LABEL_BYTE = 1,
+	  LENGTH_BYTE = 4,
+	  NEXT_HEADER_BYTE = 6,
+	  HOP_LIMIT_BYTE = 7,
+	  SOURCE_ADDRESS_BYTE= 8,
+	  DESTINATION_ADDRESS_BYTE= 24
+	};
+	
+	enum position_shift_bit
+	{
+	  VERSION_BIT = 0,
+	  TRAFFIC_CLASS_BIT = 4,
+	  FLOW_LABEL_BIT = 4,
+	  LENGTH_BIT = 0,
+	  NEXT_HEADER_BIT = 0,
+	  HOP_LIMIT_BIT = 0,
+	  SOURCE_ADDRESS_BIT= 0,
+	  DESTINATION_ADDRESS_BIT= 0
+	};
+	
+	enum length_bit
+	{
+	  VERSION_LEN = 4,
+	  TRAFFIC_CLASS_LEN = 8,
+	  FLOW_LABEL_LEN = 20,
+	  LENGTH_LEN = 16,
+	  NEXT_HEADER_LEN = 8,
+	  HOP_LIMIT_LEN = 8,
+	  SOURCE_ADDRESS_LEN= 8,
+	  DESTINATION_ADDRESS_LEN= 8
+	};
+	
+	private:
+	block_data_t header_[40];
+	block_data_t* payload_;
+	Debug& debug()
+	{ return *debug_; }
+
+	typename Debug::self_pointer_t debug_;
+	};
+}
+#endif
