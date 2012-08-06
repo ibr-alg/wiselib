24558637aaad9c90ea8f074b5cdaf616bc6009c0
Konstantinos Chantzis <chantzis.konstantinos@googlemail.com>
2012-08-06 03:17:59 -0700

    updating tracking (major changes to protocol more to come soon)

---
+++ apps/generic_apps/pltt_app/blank_app.cpp
@@ -0,0 +1,6 @@
+#include "external_interface/external_interface_testing.h"
+#include "util/serialization/simple_types.h"
+typedef wiselib::OSMODEL Os;
+void application_main( Os::AppMainParameter& value )
+{
+}
