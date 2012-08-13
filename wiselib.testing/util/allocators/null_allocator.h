632961ca2dbed84c84a9bb25731a2b289588d459
Henning Hasemann <henning@leetless.de>
2012-08-13 10:33:43 +0200

    NullAllocator provides self-defined pointer-types for now (for backwards compatibility)

--- wiselib.testing/util/allocators/null_allocator.h
+++ wiselib.testing/util/allocators/null_allocator.h
@@ -37,18 +37,79 @@ class NullAllocator {
 		typedef typename OsModel::size_t size_type;
 		typedef typename OsModel::size_t size_t;
 		typedef typename OsModel::block_data_t block_data_t;
-	
-		template<typename T>
-		T* allocate() { return (T*)0; }
 		
 		template<typename T>
-		T* allocate_array(size_type n) { return (T*)0; }
+		struct pointer_t {
+			public:
+				pointer_t() : p_(0) { }
+				pointer_t(T* p) : p_(p) { }
+				pointer_t(const pointer_t& other) : p_(other.p_) { }
+				pointer_t& operator=(const pointer_t& other) { p_ = other.p_; return *this; }
+				T& operator*() const { return *p_; }
+				T* operator->() const { return p_; }
+				T& operator[](size_t idx) { return p_[idx]; }
+				const T& operator[](size_t idx) const { return p_[idx]; }
+				bool operator==(const pointer_t& other) const { return p_ == other.p_; }
+				bool operator!=(const pointer_t& other) const { return p_ != other.p_; }
+				operator bool() const { return p_ != 0; }
+				pointer_t& operator++() { ++p_; return *this; }
+				pointer_t& operator--() { --p_; return *this; }
+                pointer_t operator + (size_t i){return pointer_t(p_+i);}
+				
+				// Only for allocator-internal use! (we need to make this
+				// public for operator new to work)
+				T* raw() { return p_; }
+				const T* raw() const { return p_; }
+			protected:
+				T* p_;
+				
+			friend class NullAllocator<OsModel_P>;
+		}; // __attribute__((__packed__));
+		template<typename T>
+		struct array_pointer_t {
+			public:
+				array_pointer_t() : p_(0) { }
+				array_pointer_t(T* p) : p_(p) { }
+				array_pointer_t(const array_pointer_t& other) : p_(other.p_) { }
+				array_pointer_t& operator=(const array_pointer_t& other) { p_ = other.p_; return *this; }
+				T& operator*() const { return *p_; }
+				T* operator->() const { return p_; }
+				T& operator[](size_t idx) { return p_[idx]; }
+				const T& operator[](size_t idx) const { return p_[idx]; }
+				bool operator==(const array_pointer_t& other) const { return p_ == other.p_; }
+				bool operator!=(const array_pointer_t& other) const { return p_ != other.p_; }
+				operator bool() const { return p_ != 0; }
+				array_pointer_t& operator++() { ++p_; return *this; }
+				array_pointer_t& operator--() { --p_; return *this; }
+                array_pointer_t operator + (size_t i){return array_pointer_t(p_+i);}
+				
+				// Only for allocator-internal use! (we need to make this
+				// public for operator new to work)
+				T* raw() { return p_; }
+				const T* raw() const { return p_; }
+			protected:
+				T* p_;
+				
+			friend class NullAllocator<OsModel_P>;
+		}; // __attribute__((__packed__));
+	
+		template<typename T>
+		pointer_t<T> allocate() { return pointer_t<T>(); }
+		
+		template<typename T>
+		array_pointer_t<T> allocate_array(size_type n) { return array_pointer_t<T>(); }
 		
 		template<typename T>
 		int free(T* p) { return OsModel::SUCCESS; }
 	
 		template<typename T>
 		int free_array(T* p) { return OsModel::SUCCESS; }
+		
+		template<typename T>
+		int free(pointer_t<T> p) { return OsModel::SUCCESS; }
+	
+		template<typename T>
+		int free_array(array_pointer_t<T> p) { return OsModel::SUCCESS; }
 };
 
 } // namespace wiselib
