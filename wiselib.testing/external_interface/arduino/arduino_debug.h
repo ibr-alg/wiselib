2dc15daedf29641d04d9f2b938c5cc9abb891322
Alin Rus <netblock@gmail.com>
2012-08-17 07:37:03 +0300

    Restrict debug message to 32 chars. Previous value of 1024 was waaaaay to big.

--- wiselib.testing/external_interface/arduino/arduino_debug.h
+++ wiselib.testing/external_interface/arduino/arduino_debug.h
@@ -47,6 +47,8 @@ namespace wiselib
          Serial.begin( 9600 );
       }
       // --------------------------------------------------------------------
+      enum Restrictions { MAXLINE = 32 };
+      // --------------------------------------------------------------------
       /**
        * Uses printf() internally.
        */
@@ -61,7 +63,7 @@ namespace wiselib
    debug( const char* msg, ... )
    {
       va_list fmtargs;
-      char buffer[1024];
+      char buffer[MAXLINE];
       va_start( fmtargs, msg );
       vsnprintf( buffer, sizeof( buffer ) - 1, msg, fmtargs );
       va_end( fmtargs );
