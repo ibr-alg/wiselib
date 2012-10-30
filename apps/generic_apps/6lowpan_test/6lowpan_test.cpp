eb9dd20fcc761a098dde961709fdae6874766326
wiselib <daniel.gehberger@gmail.com>
2012-10-30 12:49:41 +0100

    Test app updated.

--- apps/generic_apps/6lowpan_test/6lowpan_test.cpp
+++ apps/generic_apps/6lowpan_test/6lowpan_test.cpp
@@ -1,3 +1,11 @@
+/*
+* File: 6lowpan_test.cpp
+* Author: Daniel Gehberger - GSoC 2012 - 6LoWPAN project
+*
+* Test application for the IPv6 stack
+*
+*/
+
 
 #include "external_interface/external_interface.h"
 #include "algorithms/6lowpan/ipv6_address.h"
@@ -15,143 +23,117 @@ typedef Os::Radio::node_id_t node_id_t;
 #ifdef SHAWN
 typedef wiselib::ShawnTestbedserviceUartModel<wiselib::OSMODEL> Uart;
 #else
-typedef Os::B_Uart Uart;
+typedef Os::Uart Uart;
 #endif
-
 
 typedef wiselib::IPv6Stack<Os, Radio, Os::Debug, Os::Timer, Uart> IPv6_stack_t;
 
 typedef wiselib::IPv6Address<Radio, Os::Debug> IPv6Address_t;
 
-
 class lowpanApp
 {
-  public:
-    typedef Radio::block_data_t block_data_t;
-    typedef Radio::node_id_t node_id_t;
-    typedef Radio::size_t size_t;
-    typedef Radio::message_id_t message_id_t;
-    
-      void init( Os::AppMainParameter& value )
-      {
-         radio_ = &wiselib::FacetProvider<Os, Os::Radio>::get_facet( value );
-         timer_ = &wiselib::FacetProvider<Os, Os::Timer>::get_facet( value );
-         debug_ = &wiselib::FacetProvider<Os, Os::Debug>::get_facet( value );
-	 uart_ = &wiselib::FacetProvider<Os, Uart>::get_facet( value );
+public:
+	typedef Radio::block_data_t block_data_t;
+	typedef Radio::node_id_t node_id_t;
+	typedef Radio::size_t size_t;
+	typedef Radio::message_id_t message_id_t;
+	
+	void init( Os::AppMainParameter& value )
+	{
+		radio_ = &wiselib::FacetProvider<Os, Os::Radio>::get_facet( value );
+		timer_ = &wiselib::FacetProvider<Os, Os::Timer>::get_facet( value );
+		debug_ = &wiselib::FacetProvider<Os, Os::Debug>::get_facet( value );
+		uart_ = &wiselib::FacetProvider<Os, Uart>::get_facet( value );
 
-         debug_->debug( "Booting with ID: %x\n", radio_->id());
-	 
-	 ipv6_stack_.init(*radio_, *debug_, *timer_, *uart_);
-	 
-	 
-	 callback_id = ipv6_stack_.icmpv6.reg_recv_callback<lowpanApp,&lowpanApp::receive_echo_reply>( this );
-	 callback_id = ipv6_stack_.udp.reg_recv_callback<lowpanApp,&lowpanApp::receive_radio_message>( this );
-	 
-	 /*
-	 //HACK
-	 //It will have to come from an advertisement!
-	 uint8_t my_prefix[8];
-	 my_prefix[0] = 0x12;
-	 my_prefix[1] = 0x1F;
-	 my_prefix[2] = 0x1A;
-	 my_prefix[3] = 0x12;
-	 my_prefix[4] = 0x1B;
-	 my_prefix[5] = 0x1A;
-	 my_prefix[6] = 0xF2;
-	 my_prefix[7] = 0x1D;
-	 //HACK
-	 ipv6_stack_.interface_manager.set_prefix_for_interface( my_prefix, ipv6_stack_.interface_manager.INTERFACE_RADIO , 64 );
-	 */
-	 //NOTE Test IP packet
-	 IPv6Address_t sourceaddr;
-	 IPv6Address_t destinationaddr;
-	 
-	 destinationaddr.set_debug( *debug_ );
-	 
-	 
-	 destinationaddr.make_it_link_local();
-	 
-	 
-	 node_id_t ll_id = 0x2110;
-	 destinationaddr.set_long_iid(&ll_id, false);
-	 
-	 //uint8_t mypayload[] = "hello :) This is a long test message from node 0 to test the fragmentation. Google will pre-publish the evaluation questions for both students and mentors. Mentors will fill out mid-term and final evaluations for their students via the Google Summer of Code 2012 site. These evaluations will be visible in the system to the mentor and the mentoring organization s administrator(s). Students will fill out a mid-term and final evaluation of their mentors online as well, and their evaluations will only be visible in the system to the mentoring organization s administrator(s). Program administrators from Google will have access to all evaluation data. Any student who does not submit an evaluation by the evaluation deadline will fail that evaluation, regardless of the grade the mentor gives the student. If a student submits his or her evaluation on time but the mentor does not, then the student is  in an undecided state until the program administrators can speak to the mentor and determine the student s grade. Students who fail the mid-term are immediately removed from the program: it s not possible to fail the mid-term, stay in the program, and then have a final evaluation. In almost all cases, students will never see their mentor s evaluation of their progress, nor will a mentor see a student s evaluation of her/his mentorship.";
-	 
-	 /*
-	 UDP
-	 */
-	/* if( radio_->id() == 1 )
-	 {
-	 	int my_number = ipv6_stack_.udp.add_socket( 10, 10, destinationaddr, callback_id );
-	 	//ipv6_stack_.udp.print_sockets();
-	 	ipv6_stack_.udp.send(my_number,sizeof(mypayload),mypayload);
-	 }
-	 if( radio_->id() == 0 )
-	 {
-	 	node_id_t ll_id = 1;
-	 	destinationaddr.set_long_iid(&ll_id, false);
-	 	ipv6_stack_.udp.add_socket( 10, 10, destinationaddr, callback_id );
-	 }*/
-	 
-	 /*ICMPv6 Ping test*/
-	 // 0 ---> 1 <---- 2
-	 //if( radio_->id() == 0x2110 )
-	 //{
-	 	debug_->debug("Application layer: sending echo request");
-		ipv6_stack_.icmpv6.ping(destinationaddr);
-	 	//ipv6_stack_.icmpv6.ping(ipv6_stack_.ipv6.BROADCAST_ADDRESS);
-	 //}
-	 
-	 
-         //timer_->set_timer<lowpanApp,&lowpanApp::broadcast_loop>( 3000, this, 0 );
-      }
-      // --------------------------------------------------------------------
-      void broadcast_loop( void* )
-      {
-	 /*debug_->debug( "broadcasting message at %x\n", radio_->id() );
-         Os::Radio::block_data_t message[] = "ID-collision test\0";
-         radio_->send( Os::Radio::BROADCAST_ADDRESS, sizeof(message), message );*/
+		debug_->debug( "Booting with ID: %x\n", radio_->id());
+		
+		ipv6_stack_.init(*radio_, *debug_, *timer_, *uart_);
+		
+		callback_id = ipv6_stack_.icmpv6.reg_recv_callback<lowpanApp,&lowpanApp::receive_echo_reply>( this );
+		callback_id = ipv6_stack_.udp.reg_recv_callback<lowpanApp,&lowpanApp::receive_radio_message>( this );
+		
+		/*
+			Test destination
+		*/
+		uint8_t my_prefix[8];
+		my_prefix[0] = 0x20;
+		my_prefix[1] = 0x01;
+		my_prefix[2] = 0x63;
+		my_prefix[3] = 0x80;
+		my_prefix[4] = 0x70;
+		my_prefix[5] = 0xA0;
+		my_prefix[6] = 0xB0;
+		my_prefix[7] = 0x69;
 
-	 //timer_->set_timer<lowpanApp,&lowpanApp::broadcast_loop>( 3000, this, 0 );
-      }
-      // --------------------------------------------------------------------
-      void receive_radio_message( IPv6Address_t from, uint16_t len, Os::Radio::block_data_t *buf )
-      {
+		IPv6Address_t destinationaddr;
 
-         debug_->debug( "Application layer received msg at %x from ", radio_->id() );
-	 from.set_debug( *debug_ );
-	 from.print_address();
-	 debug_->debug( " Size: %i Content: ", len);
-	 //for( int i = 0; i < len; i++ )
-	 	//debug_->debug( "%c", buf[i] );
-	 debug_->debug( "\n" );
+		//destinationaddr.make_it_link_local();
+		destinationaddr.set_prefix( my_prefix, 64 );
 
-      }
-      // --------------------------------------------------------------------
-      void receive_echo_reply( IPv6Address_t from, size_t len, Os::Radio::block_data_t *buf )
-      {
-       if( len == 1 && buf[0] == ipv6_stack_.icmpv6.ECHO_REPLY )
-       {
-       		debug_->debug( "Application layer received echo reply at %x from ", radio_->id() );
-       		from.set_debug( *debug_ );
-       		from.print_address();
-       }
-       
-      }
-   private:
-      int callback_id;
+		node_id_t ll_id = 0x2140;
+		destinationaddr.set_long_iid(&ll_id, true);
+		
+		/*
+			UDP test
+		*/
+		/* if( radio_->id() == 0x2140 )
+		{
+			int my_number = ipv6_stack_.udp.add_socket( 10, 10, destinationaddr, callback_id );
+			//ipv6_stack_.udp.print_sockets();
+			uint8_t mypayload[] = "hello :) This is a test message."
+			ipv6_stack_.udp.send(my_number,sizeof(mypayload),mypayload);
+		}
+		if( radio_->id() == 0x2144 )
+		{
+			node_id_t ll_id = 1;
+			destinationaddr.set_long_iid(&ll_id, false);
+			ipv6_stack_.udp.add_socket( 10, 10, destinationaddr, callback_id );
+		}*/
+		
+		/*
+			ICMPv6 Ping test
+		*/
+		/*
+		if( radio_->id() == 0x2144 )
+		{
+			debug_->debug("Application layer: sending echo request");
+			ipv6_stack_.icmpv6.ping(destinationaddr);
+			//ipv6_stack_.icmpv6.ping(ipv6_stack_.ipv6.BROADCAST_ADDRESS);
+		}
+		*/
+	}
 
-      IPv6_stack_t ipv6_stack_;
-      Radio::self_pointer_t radio_;
-      Os::Timer::self_pointer_t timer_;
-      Os::Debug::self_pointer_t debug_;
-      Uart::self_pointer_t uart_;
-      //Os::Rand::self_pointer_t rand_;
+	// --------------------------------------------------------------------
+	void receive_radio_message( IPv6Address_t from, uint16_t len, Os::Radio::block_data_t *buf )
+	{
+		char str[43];
+		debug_->debug( "Application layer received msg at %x from ", radio_->id(), from.get_address(str) );
+		debug_->debug( " Size: %i Content: %s ", len, buf);
+	}
+
+	// --------------------------------------------------------------------
+	void receive_echo_reply( IPv6Address_t from, size_t len, Os::Radio::block_data_t *buf )
+	{
+		if( len == 1 && buf[0] == ipv6_stack_.icmpv6.ECHO_REPLY )
+		{
+			char str[43];
+			debug_->debug( "Application layer received echo reply at %x from ", radio_->id(), from.get_address(str) );
+		}
+	}
+
+private:
+	int callback_id;
+
+	IPv6_stack_t ipv6_stack_;
+	Radio::self_pointer_t radio_;
+	Os::Timer::self_pointer_t timer_;
+	Os::Debug::self_pointer_t debug_;
+	Uart::self_pointer_t uart_;
 };
 // --------------------------------------------------------------------------
 wiselib::WiselibApplication<Os, lowpanApp> example_app;
 // --------------------------------------------------------------------------
 void application_main( Os::AppMainParameter& value )
 {
-  example_app.init( value );
+	example_app.init( value );
 }
