6f221a411733b4406246279675434a80d72de62b
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-01-14 11:46:46 +0200

    upload the AdaptiveND alg

---
+++ wiselib.testing/algorithms/neighbor_discovery/adaptive/configuration.h
@@ -0,0 +1,48 @@
+/***************************************************************************
+ ** This file is part of the generic algorithm library Wiselib.           **
+ ** Copyright (C) 2008,2009 by the Wisebed (www.wisebed.eu) project.      **
+ **                                                                       **
+ ** The Wiselib is free software: you can redistribute it and/or modify   **
+ ** it under the terms of the GNU Lesser General Public License as        **
+ ** published by the Free Software Foundation, either version 3 of the    **
+ ** License, or (at your option) any later version.                       **
+ **                                                                       **
+ ** The Wiselib is distributed in the hope that it will be useful,        **
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+ ** GNU Lesser General Public License for more details.                   **
+ **                                                                       **
+ ** You should have received a copy of the GNU Lesser General Public      **
+ ** License along with the Wiselib.                                       **
+ ** If not, see <http://www.gnu.org/licenses/>.                           **
+ ***************************************************************************/
+
+/*
+ * File:   configuration.h
+ * Author: Oikonomou, Amaxilatis
+ *
+ *
+ */
+
+
+#define MAX_PG_PAYLOAD 30
+
+#define IMIN 2
+#define IMAX 8
+#define PERIOD 1000
+#define MESG_THRESHOLD 2
+
+#define DUTY_PERIOD 500
+#define DUTY_RATE 30
+
+#ifndef SHAWN        
+typedef uint8_t lqi_t;
+#else
+typedef uint16_t lqi_t;
+#endif
+
+
+//#define FIX_K
+
+#define P -1
+ 
