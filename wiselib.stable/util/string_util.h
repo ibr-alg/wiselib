4e1275fa42af3a98f323facbbfcba36973cca0ab
Henning Hasemann <henning@leetless.de>
2013-03-02 18:15:39 +0100

    added parser for n3 files

---
+++ wiselib.stable/util/string_util.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+ ** This file is part of the generic algorithm library Wiselib.           **
+ ** Copyright (C) 2008,2009 by the Wisebed (www.wisebed.eu) project.      **
+ **                                                                       **
+ ** The Wiselib is free software: you can redistribute it and/or modify   **
+ ** it under the terms of the GNU Lesser General Public License as        **
+ ** published by the Free Software Foundation, either version 3 of the    **
+ ** License, or (at your option) any later version.                       **
+ **                                                                       **
+ ** The Wiselib is distributed in the hope that it will be useful,        **
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+ ** GNU Lesser General Public License for more details.                   **
+ **                                                                       **
+ ** You should have received a copy of the GNU Lesser General Public      **
+ ** License along with the Wiselib.                                       **
+ ** If not, see <http://www.gnu.org/licenses/>.                           **
+ ***************************************************************************/
+
+#ifndef STRING_UTIL_H
+#define STRING_UTIL_H
+
+namespace wiselib {
+	
+	bool is_whitespace(char c) {
+		return (c == ' ') || (c == '\t') || (c == '\x0a') || (c == '\x0d');
+	}
+	
+	char* skip_whitespace(char* p) {
+		for( ; is_whitespace(*p); p++) { }
+		return p;
+	}
+	
+	bool is_printable(char c) {
+		unsigned char uc = (unsigned char)c;
+		return (uc >= 0x20) && (uc <= 0x7e);
+	}
+}
+
+#endif // STRING_UTIL_H
+
