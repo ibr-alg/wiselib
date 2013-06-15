6dd7ca2415367d9cd0b89e4a60783f0232ca4405
Henning Hasemann <henning@leetless.de>
2013-06-15 14:16:43 +0200

    simplified & repaired rdfprovider_test

--- apps/generic_apps/rdfprovider_test/rdfprovider_test.cpp
+++ apps/generic_apps/rdfprovider_test/rdfprovider_test.cpp
@@ -22,7 +22,7 @@ typedef uint16_t bitmask_t;
 #include <util/broker/direct_broker_protocol_command_message.h>
 #include <util/broker/protobuf_rdf_serializer.h>
 #include <util/broker/shdt_serializer.h>
-#include <util/codecs/huffman_codec.h>
+#include <algorithms/codecs/huffman_codec.h>
 #include <util/pstl/list_dynamic.h>
 #include <util/pstl/vector_dynamic.h>
 #include <util/pstl/vector_dynamic_set.h>
@@ -67,25 +67,22 @@ class App {
 	public:
 		void init(Os::AppMainParameter& amp) {
 			debug_ = &wiselib::FacetProvider<Os, Os::Debug>::get_facet(amp);
-			radio_ = &wiselib::FacetProvider<Os, Os::Radio>::get_facet(amp);
-			timer_ = &wiselib::FacetProvider<Os, Os::Timer>::get_facet(amp);
+			//radio_ = &wiselib::FacetProvider<Os, Os::Radio>::get_facet(amp);
+			//timer_ = &wiselib::FacetProvider<Os, Os::Timer>::get_facet(amp);
 			
-			broker.init(debug_);
+			dict.init(debug_);
+			//ts.init(&dict, &container, debug_);
+			codec_ts.init(&dict, &container, debug_);
+			broker.init(&codec_ts);
+			
+			//broker.init(debug_);
 			
 			//insert_large_document();
 			insert_small_documents();
 			
 			//retrieve_document("doc1");
 			
-			Tuple t;
-			t.set("ab", 0, 0, 0); //broker.get_document_mask("doc1"));
-			query_and_erase_ts(t, COL(0));
-			
-			retrieve_document("doc1");
 			test_broker();
-			test_b2b();
-			test_shdt();
-			test_protobuf();
 		}
 		
 		void ins(const char* s, const char *p, const char* o, bitmask_t bm) {
@@ -105,10 +102,14 @@ class App {
 			}
 		}
 		
+		bitmask_t doc1mask;
+		bitmask_t doc2mask;
+		bitmask_t doc3mask;	
+		
 		void insert_small_documents() {
-			bitmask_t doc1mask = broker.create_document("doc1");
-			bitmask_t doc2mask = broker.create_document("doc2");
-			bitmask_t doc3mask = broker.create_document("doc3");
+			doc1mask = broker.create_document("doc1");
+			doc2mask = broker.create_document("doc2");
+			doc3mask = broker.create_document("doc3");
 			ins("ab", "7777777", "dd", doc1mask );
 			ins("ab", "88888888", "xx", doc1mask );
 			ins("yy1", "999999999", "abx", doc1mask | doc3mask);
@@ -123,47 +124,60 @@ class App {
 		}
 		
 		void test_broker() {
-			bitmask_t doc1mask = broker.create_document("doc1");
-			bitmask_t doc2mask = broker.create_document("doc2");
-			bitmask_t doc3mask = broker.create_document("doc3");
-			recvmask = broker.create_document("recv");
+			//bitmask_t doc1mask = broker.create_document("doc1");
+			//bitmask_t doc2mask = broker.create_document("doc2");
+			//bitmask_t doc3mask = broker.create_document("doc3");
+			//recvmask = broker.create_document("recv");
 			
-			debug_->debug("masks=%x %x %x %x\n", (int)doc1mask, (int)doc2mask, (int)doc3mask, (int)recvmask);
+			debug_->debug("masks=%x %x %x\n", (int)doc1mask, (int)doc2mask, (int)doc3mask); // (int)recvmask);
 			
 			typename broker_t::Tuple t;
-			
-			ins("ab", "7777777", "dd", doc1mask );
-			ins("ab", "88888888", "xx", doc1mask | doc2mask);
-			ins("yy", "999999999", "ab", doc1mask | doc3mask);
-			
-			debug_->debug("--- iter doc1:\n");
 			broker_t::iterator iter;
+			
+			//ins("ab", "7777777", "dd", doc1mask );
+			//ins("ab", "88888888", "xx", doc1mask | doc2mask);
+			//ins("yy", "999999999", "ab", doc1mask | doc3mask);
+			
+			debug_->debug("--- iter doc1:");
 			for(iter = broker.begin_document(doc1mask); iter != broker.end_document(doc1mask); ++iter) {
 				broker_t::Tuple t = *iter;
-				debug_->debug("(%s %s %s  mask=%x)\n", t.get(0), t.get(1), t.get(2), t.bitmask());
+				debug_->debug("(%s %s %s  mask=%x)", t.get(0), t.get(1), t.get(2), t.bitmask());
 			}
 			
+			debug_->debug("--- iter doc2:");
+			for(iter = broker.begin_document(doc2mask); iter != broker.end_document(doc2mask); ++iter) {
+				broker_t::Tuple t = *iter;
+				debug_->debug("(%s %s %s  mask=%x)", t.get(0), t.get(1), t.get(2), t.bitmask());
+			}
+			
+			debug_->debug("--- iter doc3:");
+			for(iter = broker.begin_document(doc3mask); iter != broker.end_document(doc3mask); ++iter) {
+				broker_t::Tuple t = *iter;
+				debug_->debug("(%s %s %s  mask=%x)", t.get(0), t.get(1), t.get(2), t.bitmask());
+			}
+			
+			debug_->debug("--- now deleting all (ab ? ?) tuples");
 			broker_t::Tuple query;
 			query.set(0, (block_data_t*)"ab");
 			broker_t::TupleStore::iterator it = broker.tuple_store().begin(&query, (1 << 0));
-			broker.tuple_store().erase(it);
 			
-			it = broker.tuple_store().begin(&query, (1 << 0));
-			broker.tuple_store().erase(it);
-			
-			debug_->debug("--- iter doc1:\n");
-			for(broker_t::iterator iter = broker.begin_document(doc1mask); iter != broker.end_document(doc1mask); ++iter) {
-				debug_->debug("(%s %s %s  mask=%x)\n", iter->get(0), iter->get(1), iter->get(2), iter->bitmask());
+			while(it != broker.tuple_store().end()) {
+				it = broker.tuple_store().erase(it);
 			}
 			
-			query.set(0, (block_data_t*)"yy");
-			it = broker.tuple_store().begin(&query, (1 << 0));
-			broker.tuple_store().erase(it);
-			
-			debug_->debug("--- iter doc1:\n");
+			debug_->debug("--- iter doc1:");
 			for(broker_t::iterator iter = broker.begin_document(doc1mask); iter != broker.end_document(doc1mask); ++iter) {
-				debug_->debug("(%s %s %s  mask=%x)\n", iter->get(0), iter->get(1), iter->get(2), iter->bitmask());
+				debug_->debug("(%s %s %s  mask=%x)", iter->get(0), iter->get(1), iter->get(2), iter->bitmask());
 			}
+			
+			//query.set(0, (block_data_t*)"yy");
+			//it = broker.tuple_store().begin(&query, (1 << 0));
+			//broker.tuple_store().erase(it);
+			
+			//debug_->debug("--- iter doc1:\n");
+			//for(broker_t::iterator iter = broker.begin_document(doc1mask); iter != broker.end_document(doc1mask); ++iter) {
+				//debug_->debug("(%s %s %s  mask=%x)\n", iter->get(0), iter->get(1), iter->get(2), iter->bitmask());
+			//}
 		}
 		
 		void retrieve_document(char * docname) {
@@ -174,128 +188,15 @@ class App {
 			}
 		}
 	
-		void query_ts(Tuple& t, column_mask_t mask) {
-			debug_->debug("--- query\n");
-			for(broker_t::iterator iter = broker.tuple_store().begin(&t, mask); iter != broker.tuple_store().end(); ++iter) {
-				debug_->debug("(%s %s %s)\n", iter->get(0), iter->get(1), iter->get(2));
-			}
-		}
-		
-		void query_and_erase_ts(Tuple& t, column_mask_t mask) {
-			debug_->debug("--------- query: t=(%s %s %s) colmask=0x%x\n", (char*)t.get(0), (char*)t.get(1), (char*)t.get(2), mask);
-			broker_t::iterator iter = broker.tuple_store().begin(&t, mask);
-			debug_->debug("\n\n\n\n");
-			
-			for(broker_t::iterator iter = broker.tuple_store().begin(&t, mask); iter != broker.tuple_store().end() ; ) { // ++iter) {
-				debug_->debug("(%s %s %s)\n", iter->get(0), iter->get(1), iter->get(2));
-				iter = broker.tuple_store().erase(iter);
-			}
-		}
-		
-		void test_b2b() {
-			b2b.init(broker, radio_, debug_);
-			
-			typedef DirectBrokerProtocolCommandMessage<Os> CommandMessage;
-			CommandMessage message;
-			
-			// Request document
-			
-			message.set_request_type(CommandMessage::GET_DOCUMENT);
-			message.set_document_name("doc1");
-			message.set_target_document_name("recv");
-			radio_->send(Os::Radio::BROADCAST_ADDRESS, message.length(), (block_data_t*)&message);
-			//b2b.on_receive(Os::Radio::BROADCAST_ADDRESS, message.length(), (block_data_t*)&message);
-			
-			timer_->set_timer<App, &App::on_timer>(5000, this, 0);
-		}
-		
-		void test_shdt() {
-			bitmask_t doc1mask = broker.get_document_mask("doc1");
-			broker_t::iterator iter = broker.begin_document(doc1mask);
-			
-			Shdt shdt; //broker.begin_document(doc1mask), broker.end_document(doc1mask));
-			
-			const size_type bufsize = 1024;
-			block_data_t buffer[bufsize];
-			
-			// write
-			size_type written = 0;
-			written = shdt.fill_buffer(buffer, bufsize, iter, broker.end_document(doc1mask));
-			debug_->debug("written: %d\n", written);
-			debug_buffer(buffer, written, 3 * sizeof(typename Shdt::table_id_t));
-			debug_->debug("done writing.\n");
-			
-			
-			// read
-			shdt.reset();
-			block_data_t *buf = buffer;
-			size_type sz = written, read = -1;
-			while(read) {
-				broker_t::Tuple tuple;
-				read = shdt.read_buffer(tuple, buf, sz);
-				buf += read;
-				sz -= read;
-				
-				if(read) {
-					debug_->debug("read tuple: (%s %s %s)\n", (char*)tuple.get(0), (char*)tuple.get(1), (char*)tuple.get(2));
-				}
-			}
-			debug_->debug("done reading.\n");
-			
-			
-		}
-		
-		void test_protobuf() {
-			bitmask_t doc1mask = broker.get_document_mask("doc1");
-			broker_t::iterator iter = broker.begin_document(doc1mask);
-			
-			Protobuf protobuf; //broker.begin_document(doc1mask), broker.end_document(doc1mask));
-			
-			const size_type bufsize = 1024;
-			block_data_t buffer[bufsize];
-			
-			// write
-			size_type written = 0;
-			written = protobuf.fill_buffer(buffer, bufsize, iter, broker.end_document(doc1mask));
-			
-			debug_->debug("written: %d\n", written);
-			debug_buffer(buffer, written, 10);
-			debug_->debug("done writing.\n");
-			
-		}
-		
-		void debug_buffer(block_data_t* buf, size_type size, size_type cols) {
-			for(size_type i=0; i<size; i++) {
-				if(buf[i] < 0x20 || buf[i] > 0x7e) {
-					debug_->debug("    %02x ", buf[i]);
-				}
-				else {
-					debug_->debug("'%c' %02x ", (char)buf[i], buf[i]);
-				}
-				if(i % cols == (cols - 1)) {
-					debug_->debug("\n");
-				}
-			}
-			
-		}
-		
-		void on_timer(void*) {
-			debug_->debug("==== iter recv:\n");
-			for(broker_t::iterator iter = broker.begin_document(recvmask); iter != broker.end_document(recvmask); ++iter) {
-				debug_->debug("(%s %s %s  mask=%x)\n", iter->get(0), iter->get(1), iter->get(2), (int)iter->bitmask());
-			}
-			timer_->set_timer<App, &App::on_timer>(5000, this, 0);
-		}
-			
-	
 	private:
-		B2B b2b;
+		DictStore::Dictionary dict;
+		DictStore::TupleContainer container;
+		//DictStore ts;
+		PresCodecDictStore codec_ts;
 		broker_t broker;
 		bitmask_t recvmask;
 		
 		Os::Debug::self_pointer_t debug_;
-		Os::Radio::self_pointer_t radio_;
-		Os::Timer::self_pointer_t timer_;
 };
 
 wiselib::WiselibApplication<Os, App> app;
