21fd7162f49e90869423bbabc61ce1e7b337beb5
Henning Hasemann <henning@leetless.de>
2013-08-23 17:35:53 +0200

    SHDT test plotting

--- apps/generic_apps/shdt_test/plot.py
+++ apps/generic_apps/shdt_test/plot.py
@@ -12,13 +12,21 @@ from matplotlib import rc
 ## for Palatino and other serif fonts use:
 
 fig = None
-rc('font',**{'family':'serif','serif':['Palatino'], 'size': 6})
+rc('font',**{'family':'serif','serif':['Palatino'], 'size': 10})
 rc('text', usetex=True)
 
 def parse(f):
 	global data
 	
 	data = {}
+	
+	# Form:
+	# data = {
+	# 	"100": {
+	# 		"tablesize": [10, 20, 30. 40. ...],
+	# 		"ratio": [0.1, 0.2, 0.3, 0.4, ...],
+	# 	}
+	# }
 	
 	for line in f:
 		origline = line
@@ -30,7 +38,7 @@ def parse(f):
 		
 		if kv['bufsize'] not in data: data[kv['bufsize']] = { 'tablesize': [], 'ratio': []}
 		data[kv['bufsize']]['tablesize'].append(int(kv['tablesize']))
-		data[kv['bufsize']]['ratio'].append(float(kv['ratio']))
+		data[kv['bufsize']]['ratio'].append(1.0 - float(kv['ratio']))
 		
 def fig_ratio():
 	global fig
@@ -38,11 +46,38 @@ def fig_ratio():
 	
 	fig = plt.figure()
 	ax = plt.subplot(111)
-	for bufsize, d in data.items():
-		ax.plot(d['tablesize'], d['ratio'], label='bufsize='+bufsize)
+	ax.set_ylim((0, 1))
+	ax.set_xlim((10, 128))
+	
+	ax.set_xlabel('Table Size (\#entries)')
+	ax.set_ylabel('Compression Ratio')
+	
+	markers = ['^', 'D', 'o', 's']
+	
+	for (bufsize, d), m in zip(
+			sorted(data.items(), key=lambda (a, b): int(a)),
+			markers
+	):
+		ax.plot(d['tablesize'], d['ratio'], label='MTU='+bufsize, marker=' ', linestyle='-',
+				markersize=5)
+		
+	_, d = data.items()[0]
+	
+	# incontextsensing
+	#ax.plot(d['tablesize'], [1.0 - 999.0 / 7686.0] * len(d['tablesize']), label='BZIP2', linestyle='--')
+	#ax.plot(d['tablesize'], [1.0 - 951.0 / 7686.0] * len(d['tablesize']), label='GZIP', linestyle='--')
+
+	# ssp
+	#ax.plot(d['tablesize'], [1.0 - 25199.0 / 904039.0] * len(d['tablesize']), label='BZIP2', linestyle='--')
+	#ax.plot(d['tablesize'], [1.0 - 51277.0 / 904039.0] * len(d['tablesize']), label='GZIP', linestyle='--')
+		
+		
+	# 4703 / 27596  4939 /
+	ax.plot(d['tablesize'], [1.0 - 4703.0 / 27596.0] * len(d['tablesize']), label='BZIP2', linestyle='--')
+	ax.plot(d['tablesize'], [1.0 - 4939.0 / 27596.0] * len(d['tablesize']), label='GZIP', linestyle='--')
 		
 	#fig.legend()
-	ax.legend()
+	ax.legend(loc='lower right')
 	fig.savefig('ratio.pdf')
 
 data = None
